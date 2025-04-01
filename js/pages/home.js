efc9ac9790ff7c696060d54b0b62498a2dfd1f1d
Tadas <tadas.x@gmail.com>
2025-04-01 22:29:09 +0300

    initial conent

---
+++ js/pages/home.js
@@ -0,0 +1,33 @@
+//IMPORTS
+
+//EXECUTION
+
+/* HERO:start */
+/* HERO:end */
+
+/* ABOUT ME:start */
+/* ABOUT ME:end */
+
+/* SERVICES:start */
+/* SERVICES:end */
+
+/* RESUME:start */
+/* RESUME:end */
+
+/* FREELANCE:start */
+/* FREELANCE:end */
+
+/* PORTFOLIO:start */
+/* PORTFOLIO:end */
+
+/* TESTTEMONIALS:start */
+/* TESTTEMONIALS:end */
+
+/* BLOGS:start */
+/* BLOGS:end */
+
+/* CONSTACTS:start */
+/* CONSTACTS:end */
+
+/* FOOTER: start */
+/* FOOTER: end */
